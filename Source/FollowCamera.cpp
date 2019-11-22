bf1848b86bf3134e6a65ce55c2cc63a1949df723
AtkinsJM <github@joshatkins.co.uk>
2019-11-22 17:25:34 +0000

    Created FollowCamera class

---
+++ Source/FollowCamera.cpp
@@ -0,0 +1,17 @@
+#include "FollowCamera.h"
+
+FollowCamera::FollowCamera()
+{
+}
+
+FollowCamera::~FollowCamera()
+{
+}
+
+void FollowCamera::Attach()
+{
+}
+
+void FollowCamera::UpdateWorld()
+{
+}
---
+++ Source/FollowCamera.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Leadwerks.h"
+
+using namespace Leadwerks;
+
+class FollowCamera : public Actor
+{
+	public:
+		FollowCamera();
+		~FollowCamera();
+		virtual void Attach();
+		//virtual void Detach();
+		virtual void UpdateWorld();
+		//virtual void UpdateMatrix();
+		//virtual void UpdatePhysics();
+		//virtual void Wake();
+		//virtual void Sleep();
+		//virtual void Collision(Entity* entity, const Vec3& position, const Vec3& normal, float speed);
+		//virtual void PostRender();
+		//virtual void Draw();
+		//virtual void DrawEach(Camera* camera);
+		//virtual void ReceiveSignal(const std::string& inputname, Entity* sender);
+	private:
+
+};
