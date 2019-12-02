ab0cc864315d1e5af4293365f268ba415c51c298
AtkinsJM <github@joshatkins.co.uk>
2019-12-02 21:29:47 +0000

    Commented out door colour setting in code and did manually with materials.

--- Source/Door.cpp
+++ Source/Door.cpp
@@ -14,31 +14,31 @@ Door::~Door()
 void Door::Attach()
 {
 	requiredKeyType = static_cast<DoorKeyType>(String::Int(entity->GetKeyValue("requiredKeyType")));
-	Print("Required key type:");
-	Print(requiredKeyType);
 
+	/*
 	switch (requiredKeyType)
 	{
-	case DoorKeyType::RED_KEY:
-		entity->SetColor(RED_COLOUR);
-		break;
-	case DoorKeyType::YELLOW_KEY:
-		entity->SetColor(YELLOW_COLOUR);
-		break;
-	case DoorKeyType::GREEN_KEY:
-		entity->SetColor(GREEN_COLOUR);
-		break;
-	case DoorKeyType::ORANGE_KEY:
-		entity->SetColor(ORANGE_COLOUR);
-		break;
-	case DoorKeyType::PURPLE_KEY:
-		entity->SetColor(PURPLE_COLOUR);
-		break;
-	case DoorKeyType::BLUE_KEY:
-	default:
-		entity->SetColor(BLUE_COLOUR);
-		break;
+		case DoorKeyType::RED_KEY:
+			entity->SetColor(RED_COLOUR);
+			break;
+		case DoorKeyType::YELLOW_KEY:
+			entity->SetColor(YELLOW_COLOUR);
+			break;
+		case DoorKeyType::GREEN_KEY:
+			entity->SetColor(GREEN_COLOUR);
+			break;
+		case DoorKeyType::ORANGE_KEY:
+			entity->SetColor(ORANGE_COLOUR);
+			break;
+		case DoorKeyType::PURPLE_KEY:
+			entity->SetColor(PURPLE_COLOUR);
+			break;
+		case DoorKeyType::BLUE_KEY:
+		default:
+			entity->SetColor(BLUE_COLOUR);
+			break;
 	}
+	*/
 }
 
 void Door::Unlock()
